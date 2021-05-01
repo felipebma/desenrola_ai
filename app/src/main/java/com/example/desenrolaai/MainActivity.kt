5a2f873c2d1f2314c499afee7205811fd28347b1
lsm-5 <lsm5@cin.ufpe.br>
2021-04-30 22:44:10 -0300

    regra de negocio para sair do app

--- app/src/main/java/com/example/desenrolaai/MainActivity.kt
+++ app/src/main/java/com/example/desenrolaai/MainActivity.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance();
+
         this.findViewById<Button>(R.id.sign_out_button).setOnClickListener{
+            mAuth!!.signOut();
             val intent = Intent(this, LoginActivity::class.java)
             finish()
             startActivity(intent)
