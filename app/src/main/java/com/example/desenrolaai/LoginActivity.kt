477429a729a3fc105c8ea26a8bf89077b45a3630
Felipe Araújo <fbaraujo@motorola.com>
2021-04-30 21:31:57 -0300

    Add main activity

--- app/src/main/java/com/example/desenrolaai/LoginActivity.kt
+++ app/src/main/java/com/example/desenrolaai/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.example.desenrolaai
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
