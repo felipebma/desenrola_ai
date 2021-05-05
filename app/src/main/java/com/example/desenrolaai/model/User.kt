da8f067dec54fef95d3d5ce023f599c5e3973000
lsm-5 <lsm5@cin.ufpe.br>
2021-05-05 05:38:01 -0300

    inserindo usuario no room

--- app/src/main/java/com/example/desenrolaai/model/User.kt
+++ app/src/main/java/com/example/desenrolaai/model/User.kt
@@ -1,8 +1,8 @@
 package com.example.desenrolaai.model
 
 data class User (
-    val email : String? = null,
-    val name : String? = null,
+    var email : String? = null,
+    var name : String? = null,
     var latitude : Double? = 0.0,
     var longitude : Double? = 0.0,
     var products : MutableList<Product>? = ArrayList()
